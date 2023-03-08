59a3f0d2b2e345594911abff0746a8e0c52c240c
DennisFadeev <dennisfadeev@web.de>
2023-03-08 15:52:47 +0100

    play with list functions

--- playground.pl
+++ playground.pl
@@ -1,4 +1,11 @@
-%any(X, X).
 any(wer, er) :- hm(sie, ihn).
 any(er, wer).
 hm(sie, ihn).
+
+father(adam, bob).
+father(bob, charlie).
+father(charlie, dave).
+father(dave, edward).
+
+append([], L, L).
+append([X|R], L, [X|T]) :- append(R,L,T).
